235408eb62237f90550d4a950f37a18cd1f18165
chyun <och2510@gmail.com>
2025-02-18 16:51:43 +0900

    Time: 5 ms (88.57%), Space: 20.7 MB (75%) - LeetHub

--- LeetHub/Hard/0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ LeetHub/Hard/0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
+    int n, m;
     const int dy[4] = {1, -1, 0, 0};
     const int dx[4] = {0, 0, 1, -1};
-    vector<vector<int>> dist;
-    int n, m;
+    vector<vector<int>> len;
     int mx = 0;
+
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         n = matrix.size();
         m = matrix[0].size();
+        len.resize(n, vector<int>(m, -1));
+
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                bfs(i, j, matrix);
+                if (len[i][j] == -1) {
+                    dfs(i, j, matrix);
+                }
+
+                mx = max(mx, len[i][j]);
             }
         }
 
         return mx;
     }
 
-    void bfs(int ci, int cj, vector<vector<int>>& matrix) {
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-        pq.push({1, ci, cj});
-        dist.resize(n);
-        for (int i = 0; i < n; ++i) {
-            dist[i].resize(m, -1);
-        }
-        dist[ci][cj] = 0;
+    int dfs(int ci, int cj, vector<vector<int>>& matrix) {
+        if (len[ci][cj] != -1) return len[ci][cj];
 
-        while (!pq.empty()) {
-            int curD = get<0>(pq.top());
-            int curI = get<1>(pq.top());
-            int curJ = get<2>(pq.top());
-            pq.pop();
-
-            if (curD < dist[curI][curJ]) continue;
-
-            mx = max(mx, curD);
-
-            for (int z = 0; z < 4; ++z) {
-                int ni = curI + dy[z], nj = curJ + dx[z];
-                if (rangeIn(ni, nj) && matrix[ni][nj] > matrix[curI][curJ] && dist[ni][nj] < curD + 1) {
-                    dist[ni][nj] = curD + 1;
-                    pq.push({curD + 1, ni, nj});
-                }
+        int mxlen = 0;
+        for (int z = 0; z < 4; ++z) {
+            int ni = ci + dy[z], nj = cj + dx[z];
+            
+            if (rangeIn(ni, nj) && matrix[ni][nj] > matrix[ci][cj]) {
+                mxlen = max(mxlen, dfs(ni, nj, matrix));
             }
         }
+
+        return len[ci][cj] = mxlen + 1;
     }
 
     bool rangeIn(int ni, int nj) {
