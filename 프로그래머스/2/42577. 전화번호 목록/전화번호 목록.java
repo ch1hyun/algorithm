4ac6e76af11d838dccdf8617773c9e2cbdc7d0bd
chyun <och2510@gmail.com>
2025-05-17 01:01:51 +0900

    [level 2] Title: 전화번호 목록, Time: 851.02 ms, Memory: 202 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+    
+        Set<String> parsing = new HashSet<>();
+        Arrays.sort(phone_book, Collections.reverseOrder());
+        
+        for (int i = 1; i <= phone_book[0].length(); ++i) {
+            parsing.add(phone_book[0].substring(0, i));
+        }
+        
+        for (int i = 1; i < phone_book.length; ++i) {
+            if (parsing.contains(phone_book[i])) {
+                return false;
+            }
+            
+            for (int j = 1; j <= phone_book[i].length(); ++j) {
+                parsing.add(phone_book[i].substring(0, j));
+            }
+        }
+        
+        return answer;
+    }
+}
