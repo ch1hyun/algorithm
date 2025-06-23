17a3362b56478554aa8f3e2e31dc572142e90465
chyun <och2510@gmail.com>
2025-06-23 17:27:24 +0900

    [level 2] Title: 올바른 괄호, Time: 0.50 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -7,10 +7,12 @@ int openCnt = 0;
 bool solution(string s)
 {
     for (char c : s) {
+        if (openCnt < 0) return false;
+        
         if (c == '(') {
             ++openCnt;
         } else {
-            if (!openCnt) return false;
+            if (openCnt <= 0) return false;
             --openCnt;
         }
     }
